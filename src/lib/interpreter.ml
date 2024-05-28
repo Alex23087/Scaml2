ca2364bd519ce135a9a81b4c914251af0e3e88ed
Alex23087 <alessandro.scala8@gmail.com>
2024-05-28 18:28:51 +0200

    Add bop and if

--- src/lib/interpreter.ml
+++ src/lib/interpreter.ml
@@ -1,5 +1,8 @@
 open Base
 
+exception InvalidBop of string
+
+let raise_invalid_bop bop v1 v2 = raise (InvalidBop ("Cannot apply " ^ (Sexp.to_string_hum (Bop.sexp_of_t bop)) ^ " to arguments " ^ (Val.to_string v1) ^ " and " ^ (Val.to_string v2)))
 let rec eval_exp (env: Exp.t Val.t Env.t) (pc: Lbl.t) (exp: Exp.t): Exp.t Val.t =
 	match exp with
 		| Var ide -> (
@@ -12,6 +15,47 @@ let rec eval_exp (env: Exp.t Val.t Env.t) (pc: Lbl.t) (exp: Exp.t): Exp.t Val.t 
 				| _ -> (v, Lbl.join pc ell)
 		)
 		| Lit (v, _) -> (v, pc)
+		| Bop (bop, e1, e2) -> (
+			let (v1, l1) = eval_exp env pc e1 in
+			let (v2, l2) = eval_exp env pc e2 in
+			let final_label = Lbl.join (Lbl.join pc l1) l2 in
+			match (bop, v1, v2) with
+				| (Seq, _, v2) -> (v2, final_label) (* Maybe this should be (v2,l2)?*)
+				| (_, Int i1, Int i2) -> (
+					let ibop = (match bop with
+						| Addition -> Some (+)
+						| Subtraction -> Some (-)
+						| Multiplication -> Some ( * )
+						| Division -> Some (/)
+						| _ -> None) in
+					match ibop with
+						| Some bop -> (Val.Int (bop i1 i2), final_label)
+						| None -> (
+							let bbop = (match bop with
+								| Equals -> Some (=)
+								| Less -> Some (<)
+								| More -> Some (>)
+								| LessEq -> Some (<=)
+								| MoreEq -> Some (>=)
+								| _ -> None) in
+							(match bbop with
+								| Some bop -> (Val.Bool (bop i1 i2), final_label)
+								| None -> raise_invalid_bop bop v1 v2)
+						)
+				)
+				| (_, Bool b1, Bool b2) -> (
+					let bbop = (match bop with
+						| And -> (&&)
+						| Or -> (||)
+						| Equals -> Bool.(=)
+						| _ -> raise_invalid_bop bop v1 v2) in
+					((Val.Bool (bbop b1 b2)), final_label)
+				)
+				| (Addition, String s1, String s2) -> (
+					((Val.String (s1 ^ s2)), final_label)
+				)
+				| _ -> raise_invalid_bop bop v1 v2
+		)
 		| Lam (ide, body) -> (
 			((Val.Fun (env, ide, body)), pc)
 		)
@@ -24,9 +68,18 @@ let rec eval_exp (env: Exp.t Val.t Env.t) (pc: Lbl.t) (exp: Exp.t): Exp.t Val.t 
 			let (v, l) = eval_exp new_env pc body in
 			(v, Lbl.join pc l)
 		)
+		| If (guard, bthen, belse) -> (
+			let (bguard, lc) = eval_exp env pc guard in
+			let (resv, resl) = eval_exp env pc (match bguard with
+				| Val.Bool true -> bthen
+				| Val.Bool false -> belse
+				| _ -> failwith ("Nonboolean guard: " ^ (Val.to_string bguard))
+			) in
+			(resv, Lbl.join (Lbl.join pc lc) resl)
+		)
 		| Module decls -> (
 			let modlet = Aux.mod_let_desugaring decls in
-			let modclosure = eval_exp env pc modlet in
+			let modclosure = eval_exp env pc modlet in (* TODO: restrict environment based on export list *)
 			match modclosure with
 				| (Val.Fun (newenv, _, _), _l) -> (
 					(Val.Mod newenv, pc)
@@ -39,11 +92,14 @@ let rec eval_exp (env: Exp.t Val.t Env.t) (pc: Lbl.t) (exp: Exp.t): Exp.t Val.t 
 			|> Val.sexp_of_t (Exp.sexp_of_t)
 			|> Sexp.to_string_hum
 			|> Stdio.print_endline; *)
-			res
-			|> fst
-			|> Val.to_string
-			|> Stdio.print_string;
-			res
+			(
+				match res with
+					| (_, (Secret, _)) ->
+						raise Lbl.SecurityException
+					| (res, _) -> res
+						|> Val.to_string
+						|> Stdio.print_string
+			); res
 		)
 		| _ -> failwith ("Not implemented: " ^ (Exp.sexp_of_t exp |> Sexp.to_string_hum))
 
