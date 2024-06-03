2b4597250500072100005bb805eda0eb6c483fe6
Alex23087 <alessandro.scala8@gmail.com>
2024-06-03 15:52:59 +0200

    Example from the assignment document

---
+++ example/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCAML="$SCRIPT_DIR/../src/_build/default/bin/main_interpreter.exe"
+
+cd "$SCRIPT_DIR/../src"
+make build > /dev/null
+cd "$SCRIPT_DIR"
+"$SCAML" "$SCRIPT_DIR/main.scaml"
